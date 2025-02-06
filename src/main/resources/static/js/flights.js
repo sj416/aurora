131c4d37e7dcb51fea55e925fb6e8fa6f5233ec1
SD1-08 <rhtmdwls7@gmail.com>
2025-02-06 17:42:47 +0900

    기초 세팅중2

--- src/main/resources/static/js/flights.js
+++ src/main/resources/static/js/flights.js
@@ -2,6 +2,8 @@ function searchFlights() {
     const departure = document.getElementById('departure').value;
     const arrival = document.getElementById('arrival').value;
     const date = document.getElementById('date').value;
+    // const flightcode = document.getElementById('flightcode').value;
+
 
 
     // 항공편 검색 결과 예시 (실제 데이터는 API 호출로 가져오면 좋음)
@@ -39,7 +41,7 @@ function searchFlights() {
             const flightCard = document.createElement('div');
             flightCard.classList.add('flight-card');
             flightCard.innerHTML = `
-                <h3>${flight.airline} (${flight.flightNumber})</h3>
+                <h3>${flight.airline} (${flight.flightcode})</h3>
                 <p>출발지: ${flight.departure}</p>
                 <p>도착지: ${flight.arrival}</p>
                 <p>날짜: ${flight.date}</p>
