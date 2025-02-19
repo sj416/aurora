dba5b67a6ac8583b2d7d60ec4971348e54980095
SD1-08 <rhtmdwls7@gmail.com>
2025-02-19 18:56:34 +0900

    예약기능 개발중

--- src/main/java/com/aurora5/aurora/AuroraApplication.java
+++ src/main/java/com/aurora5/aurora/AuroraApplication.java
@@ -1,7 +1,6 @@
 package com.aurora5.aurora;
 
-import org.apache.juli.logging.Log;
-import org.apache.juli.logging.LogFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,28 +9,9 @@ import java.text.NumberFormat;
 @SpringBootApplication
 public class AuroraApplication {
 
-    private static Log logger = LogFactory.getLog(AuroraApplication.class);
-
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args)  {
         SpringApplication.run(AuroraApplication.class, args);
-        Runtime runtime = Runtime.getRuntime();
-
-        final NumberFormat numberFormat = NumberFormat.getInstance();
-
-        final long maxMemory = runtime.maxMemory();
-        final long allocatedMemory = runtime.totalMemory();
-        final long freeMemory = runtime.freeMemory();
-        final long mb = 1024 * 1024;
-        final String mega = "MB";
-
-        logger.info("FREE MEMORY: " + numberFormat.format(freeMemory / mb) + mega);
-        logger.info("ALLOCATED MEMORY: " + numberFormat.format(allocatedMemory / mb) + mega);
-        logger.info("MAX MEMORY: " + numberFormat.format(maxMemory / mb) + mega);
-        logger.info("TOTAL FREE MEMORY: " + numberFormat.format((freeMemory + (maxMemory - allocatedMemory)) / mb) + mega);
 
     }
-
-
 
 }
